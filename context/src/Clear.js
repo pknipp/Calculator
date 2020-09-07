5fdb8f8186167e7e9ca811622307005bd5db31bc
Peter Knipp <peteraknipp@gmail.com>
2020-09-07 18:56:30 -0400

    Remove state from Clear component

--- context/src/Clear.js
+++ context/src/Clear.js
@@ -2,35 +2,14 @@ import React from "react";
 import CalculatorContext from "./CalculatorContext";
 
 class Clear extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      x: "",
-      y: "",
-      i: 0,
-    }
-  }
-
+  constructor(props) {super(props)}
   static contextType = CalculatorContext;
-
-  handleCle = () => {
-    this.setState({x: null, z: null, y: null, i: 0});
-  }
-
-  render () {
-    debugger
-    return (
-      <button onClick={this.context.clear}>Clear</button>
-    )
-  }
+  render () {return <button onClick={this.context.clear}>Clear</button>}
 }
 
 const ClearWithContext = () => (
   <CalculatorContext.Consumer>
-    {value => {
-      debugger
-      return <Clear />
-    }}
+    {value => <Clear />}
   </CalculatorContext.Consumer>
 );
 
