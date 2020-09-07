3a7733189da191cfba00167008f4e2fb5cf1deb0
Peter Knipp <peteraknipp@gmail.com>
2020-09-07 18:50:22 -0400

    Remove state from Operation component

--- context/src/Input.js
+++ context/src/Input.js
@@ -2,18 +2,13 @@ import React from "react";
 import CalculatorContext from "./CalculatorContext";
 
 class Input extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+  constructor(props) {super(props)}
 
   static contextType = CalculatorContext;
-
   handleInput = e => this.context.updateInput(e.target.name, e.target.value);
 
   render() {
-    return (
-      <input onChange={this.handleInput} name={this.props.name} placeholder="number" value={this.props.num}/>
-    )
+    return <input onChange={this.handleInput} name={this.props.name} placeholder="number" value={this.props.num}/>
   }
 }
 
--- context/src/Operation.js
+++ context/src/Operation.js
@@ -2,31 +2,20 @@ import React from "react";
 import CalculatorContext from "./CalculatorContext";
 
 class Operation extends React.Component {
-  debugger
-  constructor(props) {
-    super(props);
-    this.state = {i: 0};
-    this.options = ["choose op","+","-","*","/"]
-  }
-
+  constructor(props) {super(props)}
   static contextType = CalculatorContext;
-
-  handleOp = async e => {
-    const val = e.target.value;
-    await this.setState({i: val}, () => this.context.updateOp(val))
-  };
-
+  handleOp = e => this.context.updateOp(e.target.value);
   render () {
+    const options = ["choose op", "+", "-", "*", "/"];
     return (
-        <select onChange={this.handleOp} value={this.props.i}>
-          {this.options.map((option, i) => <option key={i} value={i}>{option}</option>)}
-        </select>
+      <select onChange={this.handleOp} value={this.props.i}>
+        {options.map((option, i) => <option key={i} value={i}>{option}</option>)}
+      </select>
     )
   }
 }
 
 const OperationWithContext = props => {
-  debugger
   return (
   <CalculatorContext.Consumer>
     {value =>  <Operation updateOp={value.updateOp} i={props.i}/>}
