32c3b18fd03b64c33c6243b991f5154f6fa890e4
Peter Knipp <peteraknipp@gmail.com>
2020-09-08 08:01:42 -0400

    Transform Operation component from class to function

--- contextFunctions/src/Operation.js
+++ contextFunctions/src/Operation.js
@@ -1,20 +1,14 @@
 import React from "react";
 import CalculatorContext from "./CalculatorContext";
 
-class Operation extends React.Component {
-  // static contextType = CalculatorContext;
-  //handleOp = e => this.context.updateOp(e.target.value);
-  //2 lines above are an alternative - if this remains a class - to the one below
-  handleOp = e => this.props.updateOp(e.target.value);
-
-  render () {
-    const options = ["choose op", "+", "-", "*", "/"];
-    return (
-      <select onChange={this.handleOp} value={this.props.i}>
-        {options.map((option, i) => <option key={i} value={i}>{option}</option>)}
-      </select>
-    )
-  }
+const Operation = props => {
+  const options = ["choose op", "+", "-", "*", "/"];
+  const handleOp = e => props.updateOp(e.target.value);
+  return (
+    <select onChange={handleOp} value={props.i}>
+      {options.map((option, i) => <option key={i} value={i}>{option}</option>)}
+    </select>
+  )
 }
 
 const OperationWithContext = props => {
