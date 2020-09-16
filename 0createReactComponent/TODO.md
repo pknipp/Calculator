a6b6db0908f655117a185d9fbd98c44f1f1d5bb3
Peter Knipp <peteraknipp@gmail.com>
2020-09-16 15:27:20 -0400

    Last (forever?) progress w/using React.createElement

--- 0createReactComponent/TODO.md
+++ 0createReactComponent/TODO.md
@@ -1,1 +1,4 @@
 Stuff to do:
+Make a calculator w/out using jsx, but using React.createElement calls
+This'll not be possible UNLESS I can figure out how to do this w/a class (which
+is required for monitoring "state").
--- 0createReactComponent/src/App.js
+++ 0createReactComponent/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 //import Calculator from './Calculator'
 
+let x;
+const handleInput = e => {
+  const newNum = {};
+  newNum[e.target.name] = e.target.value;
+  //this.setState(newNum, this.handleCalc);
+  // Why does the following 1-liner generate an error message?
+  //this.setState({`${e.target.name}`: Number(e.target.value)}), this.handleCalc);
+}
+
 const App = () => (
   React.createElement(
     'div',
     {className: "App"},
     React.createElement('h2', null, '4-function calculator'),
-    React.createElement('input'),
+    React.createElement('input', {onChange: handleInput, name:"x", value:x}),
     React.createElement(
       "select",
       null,
